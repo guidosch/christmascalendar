0cc6359748f0a43b08e92945143ea0f5eb79886b
Guido Schnider <guido.schnider@gmail.com>
2019-11-25 21:28:36 +0100

    Playlist for Daniela finalized.

--- script.js
+++ script.js
@@ -7,16 +7,14 @@
 
 songs =
 [
-"https://open.spotify.com/embed/track/5bh8l1991KxZHgsYoV6oEx?si=XgMjmdAdSbGdKKYX3QyXlA",
 "https://open.spotify.com/embed/track/4hkL4pzAuVtpS70FzS1i5i?si=GPM3TIucTEapQA5EfS5JPg",
 "https://open.spotify.com/embed/track/76pypmPEnNfWhtuLDQH7dk?si=0sSINqCLRx-wE3dh3lugVA",
-"https://open.spotify.com/embed/track/2y8PnhXwnyHTxe72TtY58e?si=Wp5w0aB_SvazVQ1NaXrZHw",
 "https://open.spotify.com/embed/track/2Xb1md4kHUzZpnUTZ4fYC4?si=s6KP6iHCRMGOF7qOBireGg",
 "https://open.spotify.com/embed/track/0iFEdVpsCi89Wd3UjlmdMH?si=v5U18UFjR_WLSKKvrwDz7A",
 "https://open.spotify.com/embed/track/3ejU8JMenktibKkNz9dPBL?si=91LyYq-nSTOnwspOB3pwIQ",
+"https://open.spotify.com/embed/track/57DfOHOZvC7Yrk8vnG9DzM?si=XIfPebTFRraL7qHnaiNxtQ",
 "https://open.spotify.com/embed/track/4sb1gQICYmaLMulCaEAtUo?si=ZoA7qDq8Q6euPHH0GYWf8Q",
 "https://open.spotify.com/embed/track/4b1dW0Msi3ksTI3ZvFURfQ?si=RxmVx2GxQ2mBZ4OyC1xgJw",
-"https://open.spotify.com/embed/track/6MheD6qKezwyZVtuHSeGbJ?si=FtIA9_JEQDW3qpddFhdsFg",
 "https://open.spotify.com/embed/track/5CkXCkzvAbmX6JnbaDaJvd?si=sOcYdO9nSaWRQdUA1xJTrQ",
 "https://open.spotify.com/embed/track/4NxrqviMDGuKa0jmVNlVrE?si=JuyewQw1SRKQX3ELZ8VFwA",
 "https://open.spotify.com/embed/track/7eqj2U3xVj38OtdMHpN0zw?si=swHGIamDRZqzHHugQUFNaw",
@@ -26,18 +24,13 @@ songs =
 "https://open.spotify.com/embed/track/22SNmmSMXIrADD6ahmsj0N?si=Rt-O2wdLS1-_PxsMcsUlhA",
 "https://open.spotify.com/embed/track/3RJeEv9n8dP55yeHucEMxB?si=GyxW9Z2eQW-z9YllMCPDfQ",
 "https://open.spotify.com/embed/track/4oLAFHhUbUpW6Z8Ux1CPEp?si=-k7fqB3gQVG-gZ36hKPnNQ",
-"https://open.spotify.com/embed/track/766Qw2T1KAYK0yVsmdVgLo?si=UR4ZhVh5TlSLDIxAfVChag",
 "https://open.spotify.com/embed/track/3R1abI6qZDg9d3CI4oLDnf?si=gGEHBVILT9KX2DybX2UXfA",
 "https://open.spotify.com/embed/track/3HNDHljCQQiWIpagsFlpVK?si=k6jTO6duRVWoTh3wfAyTDg",
 "https://open.spotify.com/embed/track/3lIVaQHnr0s5g0aCeEcc1F?si=n0yfP6DAR7Cm7GCn4HFVew",
 "https://open.spotify.com/embed/track/5hrGOS8UP9brY2JizYc5z5?si=g7w-CSWDQJO461FVuVnl9w",
-"https://open.spotify.com/embed/track/1V4ip6ztWNqZQYAXcBWMFz?si=P8gsKPdeSsihfjagSjuy6Q",
-"https://open.spotify.com/embed/track/52xQMyCABMvx7nSoPZXeSV?si=Rlj8pmELQXmxuPDKMgiy2w",
+"https://open.spotify.com/embed/track/0NLyAAIk319L0VKYrRgnoW?si=-KALZbuRSgqTyDK2YmMJhg",
 "https://open.spotify.com/embed/track/2hZt68H0qMekKFx30gzJzH?si=EkxcCaDqSDSdiDomvXi7BA",
-"https://open.spotify.com/embed/track/6FKNqGtixJsL4Y1Qcpj7jQ?si=DKc4ApN7S8SFqzY3-qeWKg",
-"https://open.spotify.com/embed/track/15ouqiBsgL12olEwP3COsH?si=eFtHAPzmR22dvORBdbm1Dg",
-"https://open.spotify.com/embed/track/7q75JQTz2KqTtgEgIO0ULB?si=eax5GETFTn6peaxNI94jiw",
-"https://open.spotify.com/embed/track/0NLyAAIk319L0VKYrRgnoW?si=nmUnp2SfSFqgSc0m6q5hMA"
+"https://open.spotify.com/embed/track/4EHeYOIcQWpxwMgfUZs7HC?si=5PRqgks7RN2zdatoYpd4mA"
 ];
 
     
